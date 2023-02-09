69e4d4ee46eb80994f0973da7cd2879a629dafd7
lucasfernandes994 <fernandeslucassilva.developer@gmail.com>
2023-02-09 22:55:09 +0000

    Update from https://github.com/lucasfernandes994/movie_world/commit/e1b5d3ce0761a96e6cbecc6eb0735aaaacc3302d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
-"index.html": "1690da59d9a664b31e27bd8fbf338bc8",
-"/": "1690da59d9a664b31e27bd8fbf338bc8",
+"index.html": "b8d927d5d466facd26f191ea129089fa",
+"/": "b8d927d5d466facd26f191ea129089fa",
 "main.dart.js": "a2f7526c2c0f3907c5859738c58224cf",
 "manifest.json": "1ff31c64fd5b4cbc73adb670866f8b06",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
